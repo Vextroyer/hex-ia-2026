bd1e9b70431715151207fead29bf98f57c838923
vextroyer <victormanuel.vena@gmail.com>
2026-03-11 18:52:30 -0400

    Implement least cost path strategy

--- Victor_Vena_Barrios/README.md
+++ Victor_Vena_Barrios/README.md
@@ -1,18 +1,59 @@
-El algoritmo funciona mejor que el random en tableros pequenos. Juega bien incluso contra mi.
+# MCTSV1
 
-En tableros grandes (n = 20), es como un random.
-
-El algoritmo actual, que no es monte carlo pero esta basado en el.
-Para el estado actual del tablero mira todas las posibles acciones ( casillas vacias)
+El primer algoritmo, que no es monte carlo pero esta basado en el.
+Para el estado actual del tablero mira todas las posibles acciones ( casillas vacias )
 Luego mientras le quede tiempo, elige una al azar.
-Y simula una partida desde esa accion, jugando siempre al azar para ambos
+Y simula una partida desde esa accion, jugando siempre al azar para ambos.
 Entonces, anota para cada accion cuantas partida gana.
 Y devuelve la accion que gane mas partidas
+
+## Resultados
+El algoritmo funciona mejor que el random en tableros pequenos (n = 5). Juega bien incluso contra mi.
+
+En tableros grandes (n = 20), es como un random. Hace jugadas aleatorias que no lo acercan a conectar sus caminos.
+Es como si estuviese "confundido".
 
 Una mejora que se le puede hacer es acotar la cantidad de acciones inicial.
 Para esto aplicare la estrategia de los caminos de costo minimo.
 
-En resumen:
-Transforma el grafo del hex en un dag. Tiene un procedimiento especifico por capas.
-Haya los caminos de costo minimo.
-Reduce las acciones posibles a jugar en esos caminos de costo minimo.
+# Estrategia de los caminos de costo minimo:
+
+Resumen: Considera hacer tu jugada, SOLO en las casillas desocupadas que pertenecen a algun camino de costo minimo.
+Porque si consigues ocuparlas, ganaras en la menor cantidad de movimientos posibles.
+
+Para esta estrategia considera el tablero del hex como un grafo con las conexiones usuales. Donde:
+-Moverse a un nodo del otro jugador es imposible ( costo infinito),
+-Moverse a un nodo que no pertenece a ningun jugador tiene costo 1,
+-Moverse a un nodo propio tiene costo 0.
+
+Ademas el grafo tiene una fuente y un sumidero. (source and sink).
+La fuente (S) esta conectada al extremo inicial. (izquierda o superior)
+El sumidero (T) esta conectado al extremo final. (derecha o inferior)
+
+Y se busca jugar en los caminos de costo minimo de S a T.
+
+## Implementacion y complejidad
+Con una modificacion al algoritmo de Dijkstra es posible hallar los nodos que pertenecen a estos caminos.
+Se guarda para cada nodo sus "padres", es decir, sus predecesores en los distintos caminos de costo minimo.
+Despues se explora estos caminos, de hijos a padres, desde el sumidero hasta la fuente.
+Si el tamano del tablero es NxN. Hay un orden de M = N^2 nodos.
+El algoritmo de Dijkstra con una cola de prioridad toma O(ElogV), en este caso O(MlogM). La cantidad de aristas es O(6M) que es O(M).
+Y la exploracion esta acotada a O(M). Porque cada nodo solo se explora una vez.
+Por tanto la complejidad de esta estrategia es de O(MlogM)
+
+# MCTSV2
+
+El mismo algoritmo que antes. Con un cambio.
+
+Solo considera como acciones jugar en nodos que pertenecen a algun camino de costo minimo.
+
+## Resultados
+El algoritmo mantiene el rendimiento contra el random en tableros pequenos. (n = 5)
+
+Y en tableros grandes ya no se confunde. Al contrario, se concentra. Es como si jugara en una linea recta.
+En tableros con n = 20, consistentemente derrota al random en 20 jugadas. Teoricamente lo mejor.
+
+Pero.
+
+Cuando existen varios nodos para ocupar. Y alguno de ellos es critico, porque si el oponente lo ocupa el costo del
+camino de costo minima aumentara. EL algoritmo decide aleatoriamente y no se da cuenta de ese "eslabon debil". 
--- Victor_Vena_Barrios/solution.py
+++ Victor_Vena_Barrios/solution.py
@@ -2,6 +2,7 @@ from player import Player
 from board import HexBoard
 import random as random
 import time as time
+import heapq as heapq
 
 # Smart Player is a MonteCarloTreeSearch based player.
 class SmartPlayer(Player):
@@ -46,7 +47,7 @@ class SmartPlayer(Player):
     def Simulate(self,board: HexBoard, player):
         """ board: A COPY of the current board. player: the current player"""
         while not self.IsTerminal(board):
-            actions = self.GetCandidateActions(board,player)
+            actions = self.GetEmptyCells(board,player)
             action = random.choice(actions)
             self.ApplyAction(action,board)
             player = self.AlternatePlayer(player)
@@ -63,8 +64,129 @@ class SmartPlayer(Player):
             return 2
 
 
-    # Just return empty cells
+
+    # for moving in the graph
+    adyacents_for_odd_rows = [(-1,0),(0,1),(1,0),(1,-1),(0,-1),(-1,-1)]
+    adyacents_for_even_rows = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(0,-1)]
+    def Adyacent(self,row,col,size):
+        a = []
+        adj = 0
+        if row % 2 == 0:
+            adj = self.adyacents_for_even_rows
+        else:
+            adj = self.adyacents_for_odd_rows
+        for i,j in adj:
+            nrow = row + i
+            ncol = col + j
+            if nrow < 0 or nrow >= size or ncol < 0 or ncol >= size:
+                continue
+            a.append((nrow,ncol))
+        return a
+
+    # Apply policy of nodes on minimum cost paths
     def GetCandidateActions(self,board: HexBoard, player_id):
+        # dont modify the board
+        inf = 100000000
+        minimumCost = [ [inf for _ in range(board.size)] for _ in range(board.size) ]
+        parents = [ [ [] for _ in range(board.size)] for _ in range(board.size) ]
+        isExamined = [ [False for _ in range(board.size)] for _ in range(board.size) ]
+
+        # Priority queue
+        pq = []
+        
+        if player_id == 1:
+            # Get first column cells that are empty or belong to you
+            for row in range(board.size):
+                if board.board[row][0] == 0 or board.board[row][0] == 1:
+                    heapq.heappush(pq,(1 - board.board[row][0],row,0,(-1,-1)) )
+
+        if player_id == 2:
+            # Get first column cells that are empty or belong to you
+            for col in range(board.size):
+                if board.board[0][col] == 0:
+                    heapq.heappush(pq,(1,0,col,(-1,-1)))
+                if board.board[0][col] == 2:
+                    heapq.heappush(pq,(0,0,col,(-1,-1)))
+
+        # Dijkstra
+        while len(pq) > 0:
+            cost,row,col,parent = heapq.heappop(pq)
+
+            # Revisiting, ignore
+            if cost > minimumCost[row][col]:
+                continue
+
+            # Another path, different from previous one
+            if cost == minimumCost[row][col]:
+                parents[row][col].append(parent)
+                continue
+            
+            # Visited for first time
+            if cost < minimumCost[row][col]:
+                minimumCost[row][col] = cost
+                parents[row][col].append(parent)
+                for newrow,newcol in self.Adyacent(row,col,board.size):
+                    weight = 0 # If you played on this cell the edge has weight 0
+
+                    # if this cell is empty the edge has weight 1
+                    if board.board[newrow][newcol] == 0:
+                        weight = 1
+                    
+                    # You cant move to other player cells
+                    if board.board[newrow][newcol] != 0 and board.board[newrow][newcol] != player_id:
+                        continue
+                    
+                    heapq.heappush(pq,(cost + weight,newrow,newcol,(row,col)))
+
+        selected = []
+        if player_id == 1:
+            lastCol = board.size - 1
+            # Get the last column cells whose cost is equal to the minimum cost, they can be unocupied or ocupied
+            min = inf
+            for row in range(board.size):
+                if board.board[row][lastCol] == 2:
+                    continue
+                cost = minimumCost[row][lastCol]
+                if cost == min:
+                    selected.append((row,lastCol))
+                if cost < min:
+                    min = cost
+                    selected = [ (row,lastCol) ]
+                
+        if player_id == 2:
+            lastRow = board.size - 1
+            # Get the last row cells whose cost is equal to the minimum cost, they can be unocupied or ocupied by you
+            min = inf
+            for col in range(board.size):
+                if board.board[lastRow][col] == 1:
+                    continue
+                cost = minimumCost[lastRow][col]
+                if cost == min:
+                    selected.append((lastRow,col))
+                if cost < min:
+                    min = cost
+                    selected = [ (lastRow,col) ]
+
+        candidates = []
+        while len(selected) > 0:
+            row,col = selected.pop()
+            # que puede pasar sean 0 o mias, no pueden ser del otro
+            # sea -1 -1 que no se juega
+            if row == -1 or col == -1:
+                continue
+
+            if isExamined[row][col]:
+                continue
+
+            isExamined[row][col] = True
+            if board.board[row][col] == 0:
+                candidates.append((row,col,player_id))
+
+            selected = selected + parents[row][col]
+
+        return candidates
+
+    def GetEmptyCells(self,board,player_id):
         actions = []
         for row in range(board.size):
             for col in range(board.size):
